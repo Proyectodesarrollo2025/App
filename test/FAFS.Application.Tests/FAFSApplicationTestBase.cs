dc5efe82f7e342a3fc8110d43efb18ed68e61a18
Santi <allaudsantiago@gmail.com>
2025-11-17 11:20:00 -0300

    Initial upload of the project structure

---
+++ test/FAFS.Application.Tests/FAFSApplicationTestBase.cs
@@ -0,0 +1,15 @@
+﻿using Volo.Abp.Modularity;
+using Volo.Abp.Testing;
+
+namespace FAFS;
+
+// Base GENÉRICA (no la uses directamente en los tests)
+public abstract class FAFSApplicationTestBase<TStartupModule> : AbpIntegratedTest<TStartupModule>
+    where TStartupModule : IAbpModule
+{
+}
+
+// Base NO GENÉRICA (esta es la que heredan tus tests)
+public abstract class FAFSApplicationTestBase : FAFSApplicationTestBase<FAFSApplicationTestModule>
+{
+}
